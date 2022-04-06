63eea049a4acb640bc84ca0ee2eb3dd8345c3595
Bullfinch38 <100191544+Bullfinch38@users.noreply.github.com>
2022-04-06 19:31:03 +0300

    Lesson 7

---
+++ konstantin_snigirev_dz_7_3.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+
+dir_name = 'my_project_2'
+if not os.path.exists(dir_name):
+    os.mkdir(dir_name)
+
+folders = ['settings', 'adminapp', 'templates', 'authapp']
+
+for f in folders:
+    folder = os.path.join(dir_name, f)
+    os.makedirs(folder)
+
+a = shutil.move('my_project_2/adminapp', 'my_project_2/templates')
+b = shutil.move('my_project_2/authapp', 'my_project_2/templates')
+
+
+
